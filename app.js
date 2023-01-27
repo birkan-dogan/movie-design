6242cdebb431215c67cf8e5348731036d615620d
birkan <b.561birkan@gmail.com>
2023-01-27 20:44:18 +0300

    dark-mode toggle

--- app.js
+++ app.js
@@ -18,3 +18,16 @@ arrows.forEach((arrow, index) => {
     }
   });
 });
+
+const ball = document.querySelector(".toggle-ball");
+
+const items = document.querySelectorAll(
+  ".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
